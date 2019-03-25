7be6979898c362d979c33e73395de8c408e837f8
Alex Freeman <afreeman262@gmail.com>
2019-03-25 13:56:13 -0400

    Second Test + Classes Start
    Testing the process of commiting and pushing again, also got started on making the different character classes.

---
+++ DesignII-Project2/DesignII-Project2/Assets/Scripts/Knight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : GenericPlayerChar
+{
+
+	public Knight()
+    {
+        _maxHealth = 80;
+
+        _attack = 80;
+        _defense = 90;
+        _initiative = 50;
+        _accuracy = 75;
+    }
+
+    public void Slash(GenericEnemy enemyTohurt)
+    {
+        //To be implemented, probably when enemies are made
+    }
+
+    public void Shield(GenericPlayerChar charToShield)
+    {
+        //To be implemented, probably once we have the turn order sorted out
+    }
+}
