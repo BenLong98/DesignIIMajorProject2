ebe7c07c238c301d9383572561111840673cf533
LabUser <LabUser@waketech.edu>
2019-04-01 10:30:23 -0400

    Finalizing Character Selection and Beginning Battle Scene UI
    Changes:
    - Added ability to click any of the character cards and they will highlight. They will de-highlight if re-pressed
    - Added basic UI elements in Battle Scene
    - Edited Scripts: UIController
    - Added Scripts: CharacterSelection

---
+++ DesignII-Project2/Assets/Scripts/UI/CharacterSelection.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [Header("HighlightPanels")]
+    public GameObject spot1HPanel;
+    public GameObject spot2HPanel;
+    public GameObject spot3HPanel;
+    public GameObject spot4HPanel;
+    public GameObject spot5HPanel;
+    public GameObject spot6HPanel;
+
+    [SerializeField] GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    [SerializeField] EventSystem m_EventSystem;
+
+
+    void CheckActivity(GameObject panel)
+    {
+        if (panel.activeInHierarchy == true)
+        {
+            panel.SetActive(false);
+        }
+        else
+        {
+            panel.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+            m_PointerEventData.position = Input.mousePosition;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+            //Debug.Log(results[0]);
+            m_Raycaster.Raycast(m_PointerEventData, results);
+
+            foreach (RaycastResult result in results)
+            {
+                if (result.gameObject.transform.tag == "Spot1")
+                {
+                    CheckActivity(spot1HPanel);
+                }
+                if (result.gameObject.transform.tag == "Spot2")
+                {
+                    CheckActivity(spot2HPanel);
+                }
+                if (result.gameObject.transform.tag == "Spot3")
+                {
+                    CheckActivity(spot3HPanel);
+                }
+                if (result.gameObject.transform.tag == "Spot4")
+                {
+                    CheckActivity(spot4HPanel);
+                }
+                if (result.gameObject.transform.tag == "Spot5")
+                {
+                    CheckActivity(spot5HPanel);
+                }
+                if (result.gameObject.transform.tag == "Spot6")
+                {
+                    CheckActivity(spot6HPanel);
+                }
+
+            }
+        }
+    }
+}
---
+++ DesignII-Project2/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [Header("CharacterSelectionExclusiveUI")]
+    public Button startBattleButton;
+
+    [Header("BattleSceneExclusiveUI")]
+    public Button attack1Button;
+    public Button attack2Button;
+    public Text resultsText;
+
+    [Header("UniversalUI")]
+    public Button quitButton;
+    public Button gearButton;
+    public Button mainMenuButton;
+    public GameObject gearPanel;
+
+    void Start ()
+    {
+		
+	}
+
+    public void OnClickGearButton()
+    {
+        if(gearPanel.activeInHierarchy == true)
+        {
+            gearPanel.SetActive(false);
+        }
+        else
+        {
+            gearPanel.SetActive(true);
+        }
+    }
+
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+
+
+    public void OnClickCreditsButton()
+    {
+        SceneController.instance.CreditsSceneProgression();
+    }
+
+    public void OnClickStartBattleButton()
+    {
+        SceneController.instance.BattleSceneProgression();
+    }
+
+    public void OnClickMainMenuButton()
+    {
+        SceneController.instance.MainMenuSceneProgression();
+    }
+
+}
