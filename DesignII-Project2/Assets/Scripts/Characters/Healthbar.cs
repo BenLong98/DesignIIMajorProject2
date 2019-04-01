10eacac2515c8301e0530002164cd4a77b447fda
LabUser <LabUser@waketech.edu>
2019-04-01 14:56:11 -0400

    Changed Health Text to Bars
    Changes:
    - Edited HealthBar Script to change to bars instead of text

--- DesignII-Project2/Assets/Scripts/Characters/Healthbar.cs
+++ DesignII-Project2/Assets/Scripts/Characters/Healthbar.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour {
 
     private GenericPlayerChar _character;
-    private Text _healthText;
-    [SerializeField] private Text _textPrefab;
+    //private Text _healthText;
+    private Slider _healthSlider;
+    //[SerializeField] private Text _textPrefab;
+    [SerializeField] private Slider _sliderPrefab;
     [SerializeField] private bool _isEnemy = false;
 
     // Use this for initialization
@@ -15,24 +17,29 @@ public class Healthbar : MonoBehaviour {
         _character = gameObject.GetComponent<GenericPlayerChar>();
         if (!_isEnemy)
         {
-            _healthText = Instantiate(_textPrefab, transform.position + new Vector3(-1.4f, 2, 0), Quaternion.identity);
+            //_healthText = Instantiate(_textPrefab, transform.position + new Vector3(-1.4f, 2, 0), Quaternion.identity);
+            _healthSlider = Instantiate(_sliderPrefab, transform.position + new Vector3(-1.4f, 2, 0), Quaternion.identity);
         }
         else
         {
-            _healthText = Instantiate(_textPrefab, transform.position + new Vector3(1.4f, 2, 0), Quaternion.identity);
+            //_healthText = Instantiate(_textPrefab, transform.position + new Vector3(1.4f, 2, 0), Quaternion.identity);
+            _healthSlider = Instantiate(_sliderPrefab, transform.position + new Vector3(1.4f, 2, 0), Quaternion.identity);
         }
-        _healthText.transform.parent = GameObject.Find("Canvas").transform;
-        _healthText.text = gameObject.GetComponent<GenericPlayerChar>().health.ToString();
-	}
+        //_healthText.transform.parent = GameObject.Find("Canvas").transform;
+        //_healthText.text = gameObject.GetComponent<GenericPlayerChar>().health.ToString();
+        _healthSlider.transform.parent = GameObject.Find("Canvas").transform;
+        _healthSlider.value = gameObject.GetComponent<GenericPlayerChar>().health / 100;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        _healthText.text = gameObject.GetComponent<GenericPlayerChar>().health.ToString();
+        //_healthText.text = gameObject.GetComponent<GenericPlayerChar>().health.ToString();
+        _healthSlider.value = gameObject.GetComponent<GenericPlayerChar>().health / 100;
     }
 
     private void OnDisable()
     {
-        _healthText.text = "";
+        //_healthText.text = "";
     }
 }
